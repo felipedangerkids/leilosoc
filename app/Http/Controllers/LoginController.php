a4240adce98fd73cef39361235523308cd92467e
felipe cruz <felipephplow@gmail.com>
2021-07-02 15:33:06 -0300

    a

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller
     {
         if (Auth::guard('web')->attempt(['email' => $request->email, 'password' => $request->password])) {
 
-            return redirect()->intended('home');
+            return redirect()->intended('/');
         }
         return redirect('entrar');
     }
