2043f195f43e831872b82143c73db4ff89629cd3
felipe cruz <felipephplow@gmail.com>
2021-07-07 11:11:07 -0300

    a

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::middleware(['auth'])->group(function () {
     Route::any('painel/users/delete/{id}', [UserController::class, 'destroy'])->name('painel.users.delete');
     Route::post('painel/users/store', [UserController::class, 'store'])->name('painel.users.store');
 
-<<<<<<< Updated upstream
+
     Route::get('painel/departamentos', [DepertamentoController::class, 'index'])->name('painel.departamento');
     Route::get('painel/departamentos/edit/{id}', [DepertamentoController::class, 'edit'])->name('painel.departamento.edit');
     Route::post('painel/departamentos/update/{id}', [DepertamentoController::class, 'update'])->name('painel.departamento.update');
@@ -47,10 +47,4 @@ Route::middleware(['auth'])->group(function () {
     Route::any('painel/departamentos/delete/{id}', [DepertamentoController::class, 'destroy'])->name('painel.departamento.delete');
 
 });
-=======
-Route::get('painel/departamentos', [DepertamentoController::class, 'index'])->name('painel.departamento');
-Route::post('painel/departamentos/store', [DepertamentoController::class, 'store'])->name('painel.departamento.store');
 
-
-
->>>>>>> Stashed changes
